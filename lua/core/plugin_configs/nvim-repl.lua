e53d05cc5117a4fa7007a451dbb73b5b46ff3d2a
timrecyclist <tim@recyclist.co>
2023-03-22 12:23:25 -0700

    Fix keymapping for nvim-repl

--- lua/core/plugin_configs/nvim-repl.lua
+++ lua/core/plugin_configs/nvim-repl.lua
@@ -2,7 +2,7 @@ vim.cmd [[ let g:repl_filetype_commands = {'javascript': 'node', 'python': 'ipyt
 
 vim.cmd [[let g:repl_split = 'bottom']]
 
-local opts = {buffer=0, noremap=true}
+local opts = {noremap=true}
 vim.keymap.set({'n'}, '<leader><leader><cr>', '<cmd>ReplToggle<cr>', opts)
 vim.keymap.set({'n'}, '<leader><cr>', '<Plug>ReplSendLine', opts)
 vim.keymap.set({'v'}, '<leader><cr>', '<Plug>ReplSendVisual', opts)
