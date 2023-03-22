2a2e8462e433a7590b9380d649930f6c9b136b8e
timrecyclist <tim@recyclist.co>
2023-03-22 12:10:41 -0700

    Adjust main init. Order is super important

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
--- Plug-in free modules
+-- Non-plugin stuff
 require("core.keymaps")
 require("core.options")
 require("core.utils")
 
--- IMPORTANT: You need to source lazy before sourcing anything plug-in related
+-- IMPORTANT: source lazy first
 require("core.lazy")
 require("core.colors")
 require("core.autocmds")
+require("core.plugin_configs.nvim-repl")
+require("core.plugin_configs.neo-tree")
+require("core.plugin_configs.telescope")
 -- neodev needs to come before lsp
 require("core.plugin_configs.neodev")
 require("core.plugin_configs.lsp")
-require("core.plugin_configs.treesitter")
-require("core.plugin_configs.cmp")
-require("core.plugin_configs.nvim-repl")
-require("core.plugin_configs.neo-tree")
