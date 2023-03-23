66a089175999f8deef15754e721c7363204f00b3
timrecyclist <tim@recyclist.co>
2023-03-22 18:41:16 -0700

    Add keymap to autoload modules in ipython

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -37,3 +37,6 @@ vim.keymap.set({"n"}, "<leader>gg", "<cmd>tabnew | terminal gitui<cr>a", default
 
 -- Protip: use :verbose nmap to see all keymappings and where they were set from
 -- Or :Telescope keymap
+
+-- Ipython autoreload modules
+vim.keymap.set({"n"}, "<leader>ipy", "<esc>a%load_ext autoreload<cr>%autoreload 2<cr>%reload_ext autoreload<cr>", default_opts)
