cf609a8060bc07cc7991bdd12d973b3d9f187cba
timrecyclist <tim@recyclist.co>
2023-03-25 05:05:13 -0700

    Add format on save

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,7 +1,21 @@
+-- augroup
+local function augroup(group_name)
+  return vim.api.nvim_create_augroup( group_name, {clear = true})
+end
+
 -- Autocommand to make a smoother close of terminal from gitui
-vim.cmd[[ 
-augroup gitui
-  autocmd!
-  autocmd TermClose term://*gitui lua vim.api.nvim_input("<CR>")
-augroup END
-]]
+vim.api.nvim_create_autocmd("TermClose",{
+  group = augroup("gitui"),
+  callback = function()
+    vim.api.nvim_input("<cr>")
+  end
+})
+
+-- Format python scripts on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup("python_formatting"),
+  buffer = 0,
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
