a6ab27506edb4695855c6633051b1bc00e2510ea
timrecyclist <tim@recyclist.co>
2023-04-09 11:39:20 -0700

    Add borders to lsp windows

--- lua/core/plugin_configs/lsp/init.lua
+++ lua/core/plugin_configs/lsp/init.lua
@@ -4,3 +4,12 @@ require("core.plugin_configs.lsp.cmp")
 require("core.plugin_configs.lsp.treesitter")
 require("core.plugin_configs.lsp.null-ls")
 
+-- Add borders to windows
+local win = require('lspconfig.ui.windows')
+local _default_opts = win.default_opts
+
+win.default_opts = function(options)
+  local opts = _default_opts(options)
+  opts.border = 'double'
+  return opts
+end
