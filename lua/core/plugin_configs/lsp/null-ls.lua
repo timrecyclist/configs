fff6160bc6e486a6c2d3807740efbb32f32ae802
timrecyclist <tim@recyclist.co>
2023-04-09 11:40:49 -0700

    Add a bunch of formatters for null-ls

--- lua/core/plugin_configs/lsp/null-ls.lua
+++ lua/core/plugin_configs/lsp/null-ls.lua
@@ -2,14 +2,49 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
+    -- Completion
+      -- Lua
+      null_ls.builtins.completion.luasnip,
 
-        --python
+
+    -- Formatters
+      --Python
+        -- black
         null_ls.builtins.formatting.black.with({
           extra_args = {"--line-length=80"}
         }),
+        -- pyflakes
+        null_ls.builtins.formatting.autoflake,
+        -- autopep8
+        null_ls.builtins.formatting.autopep8,
+        -- isort - sorts imports
         null_ls.builtins.formatting.isort,
+        -- pyflyby - autoimport libraries
+        null_ls.builtins.formatting.pyflyby,
+
+      -- Bash
+        null_ls.builtins.formatting.beautysh,
+
+
+      -- JavaScript
+        -- eslint_d
+        null_ls.builtins.formatting.eslint_d,
+
+      -- JSON
+        -- fixjson
+        null_ls.builtins.formatting.fixjson,
+
+      -- Lua
+        -- lua_format
+        null_ls.builtins.formatting.lua_format,
+        null_ls.builtins.formatting.stylua,
+
+      -- SQL
+        -- pg_format
+        null_ls.builtins.formatting.pg_format,
+
+      -- Prettier - looks good for web dev/markdown
+      null_ls.builtins.formatting.prettierd,
     },
+
 })
