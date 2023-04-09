2f280a5eaca5464695177c91eed49786ea832ed5
timrecyclist <tim@recyclist.co>
2023-04-09 11:45:58 -0700

    Practice utils file

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,14 +1,20 @@
 -- Utils Module
+-- Using the table M allows us to keep things out of the global scope and to
+-- use only what we need when calling the function from nvim
 local M = {}
 
-function M.map(mode, lhs, rhs, opts)
-	-- set default options
-	local options = {noremap=true, silent=true}
-	if opts then
-		-- if opts provided, overwrite options
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.keymap.set(mode, lhs, rhs, opts)
+local api = vim.api
+
+function M.makeScratch()
+  -- We can create a new buffer with the enew command, and the neovim API gives
+  -- us a way to call nvim commands from lua:
+  api.nvim_command('enew') -- equivalent to :enew
+
+  -- Next we want to set some buffer options so that our scratch buffer isn't
+  -- listed in the buffer list and doesn't have a swapfile created
+  vim.bo[0].buftype='nofile'  -- set the current buffer's (buffer 0) buftype to nofile
+  vim.bo[0].bufhidden='hide'
+  vim.bo[0].swapfile=false
 end
 
 return M
